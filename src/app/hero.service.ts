dbf04f4166d89c846a3422af526859e195c9ebd6
robbiemcarthur <robbiemca123@gmail.com>
2021-10-12 16:56:00 +0100

    navigation section complete

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -16,4 +16,13 @@ export class HeroService {
     this.messageService.add('HeroService: fetched heroes');
     return heroes;
   }
+
+  getHero(id: number): Observable<Hero> {
+    // For now, assume that a hero with the specified `id` always exists.
+    // Error handling will be added in the next step of the tutorial.
+    // tslint:disable-next-line:no-non-null-assertion
+    const hero = HEROES.find(h => h.id === id)!;
+    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    return of(hero);
+  }
 }
